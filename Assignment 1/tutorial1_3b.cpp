d13555b58edf848b8b325a78407f79845ee7804d
Supratik002 <165566366+Supratik002@users.noreply.github.com>
2024-08-14 11:48:45 +0530

    Add files via upload

---
+++ Assignment 1/tutorial1_3b.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+	char capVowel1, capCowel2,capVowel3,capVowel4,capVowel5;
+	std::cout<<"Enter vowels:\n";
+	std::cin>>capVowel1>>capVowel2>>capVowel3>>capVowel4>>capVowel5;
+	int x=capVowel5-capVowel1;
+	std::cout<<"x="<<x<<"\n";
+	}
