a717144d35a38b82004c7648905b9957fb8f0d0d
Supratik002 <165566366+Supratik002@users.noreply.github.com>
2024-08-21 12:21:20 +0530

    Update tutorial1_2.cpp

--- Assignment 1/tutorial1_2.cpp
+++ Assignment 1/tutorial1_2.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-int main(){
-	float x,y;
-	std::cout<<"x=\n";
+#include<iostream>
+int main()
+{
+	double x,y;
+	std::cout<<"enter number\n";
 	std::cin>>x;
-	y=x+(1/(x+(1/(x+1/x))));
-	std::cout<<"y="<<y<<"\n";
+	if(x==0.0)
+	{
+		std::cout<<"change x";
 	}
+	else
+	{
+		y=x+1/(x+(1/(x+1/x)));
+		std::cout<<"the ans is "<< y <<"\n";
+	}
+}
