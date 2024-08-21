be9dce1c4b59341d79c8ead5c79e20660cccbaa1
Supratik002 <165566366+Supratik002@users.noreply.github.com>
2024-08-21 12:59:26 +0530

    Update tutorial1_3c.cpp

--- Assignment 1/tutorial1_3c.cpp
+++ Assignment 1/tutorial1_3c.cpp
@@ -3,6 +3,15 @@ int main(){
 	float x,y;
 	std::cout<<"enter y="<<"\n";
 	std::cin>>y;
+	x=y-20*6/9;
+	std::cout<<"x="<<x<<"\n";
 	x=y-20*6./9;
 	std::cout<<"x="<<x<<"\n";
 	}
+
+/*After Evaluating the following expression in my program:"x=y-20 *6/9", by taking some value of y and 
+  printing  the  value  of  x,I didn't see the  expected  result. 
+The floating point value of x (the correct result) didn't appear. 
+Only integer part of the answer appeared in the terminal.
+
+Therefore, I brought about minimum change(s) at the 8th line to modify my program to make it print the correct result.*/
