75d51dd5066863c2df59af3d349316d22d935efa
Supratik002 <165566366+Supratik002@users.noreply.github.com>
2024-08-21 11:04:56 +0530

    Update tutorial1_4.cpp

--- Assignment 1/tutorial1_4.cpp
+++ Assignment 1/tutorial1_4.cpp
@@ -1,30 +1,22 @@
-#include <iostream>
-int printBinary(int number, int bits) {
-    char binary[bits];
-    for (int i = 0; i < bits; ++i) {
-        binary[i] = '0';
-    }
-    for (int i = bits - 1; number > 0 && i >= 0; --i) {
-        binary[i] = (number % 2) ? '1' : '0'; 
-        number /= 2; 
-    }
-    for (int i = 0; i < bits; ++i) {
-        std::cout << binary[i];
-    }
-    return 0;
+#include<iostream>
+int main()
+{
+	int n,i;
+	std::cout<<"enter number \n";
+	std::cin>>n;
+	unsigned int m=1 <<(sizeof(int)*8-1);
+	std::cout<<"binary is\n";
+	for(i=0; i<sizeof(int)*8; i++)
+	{
+		if(n&m)
+		{
+			std::cout<<"1";
+		}
+		else
+		{
+			std::cout<<"0";
+		}
+		m >>=1;
+	}
 }
-int main() {
-    int positiveInteger, negativeInteger;
-    std::cout << "Enter a positive integer: ";
-    std::cin >> positiveInteger;
-    std::cout << "Enter a negative integer: ";
-    std::cin >> negativeInteger;
-    std::cout << "\nPositive Integer: " << positiveInteger<<"\n";
-    std::cout << "Binary Representation (8 bits): ";
-    printBinary(positiveInteger, 8);
-    std::cout << "Negative Integer: " << negativeInteger << "\n";
-    std::cout << "Binary Representation (8 bits): ";
-    printBinary(negativeInteger + 256, 8);
-    
-   }
 
