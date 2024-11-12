a5d9ba87982907010901f67cdc98b8dc33b091ef
Supratik002 <165566366+Supratik002@users.noreply.github.com>
2024-11-12 21:30:12 +0530

    Update sum_binary.cpp

--- Assignment 9/sum_binary.cpp
+++ Assignment 9/sum_binary.cpp
@@ -18,7 +18,7 @@ uint64_t sumBytesInFile(const std::string& filePath) {
     return total;
 }
 int main() {
-    std::string filePath = "your_large_file.bin";
+    std::string filePath = "my_file.txt";
     uint64_t totalSum = sumBytesInFile(filePath);
     std::cout << "The total sum of all bytes in the file is: " << totalSum << std::endl;
     return 0;
